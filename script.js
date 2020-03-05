d378e72c98bd30f59b48da4b16a78176b28e92a9
Endaytrer <endaytrer@gmail.com>
2020-03-05 22:21:52 +0800

    add a welcome screen

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+var sents = ["So live a live you will remember.", "Hello, world!","Talk is cheap. Show me the code.","Do not go gentle into that good night."];
+var by = ["Tim Bergling","Ada Lovelace","Linus Torvalds","Dylan Thomas"]
+frame = document.getElementById("welcome");
+title = document.getElementById("startify");
+author = document.getElementById("author");
+var rand=Math.floor(Math.random()*sents.length);
+title.innerHTML = '"'+sents[rand]+'"';
+author.innerHTML = by[rand];
+}
