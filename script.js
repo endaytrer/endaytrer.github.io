973d070494d811fc2661b1acbd1c21bc210a4ba4
Endaytrer <endaytrer@gmail.com>
2020-03-06 15:45:11 +0800

    change size of background

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var sents = ["So live a live you will remember.", "Hello, world!", "Talk is cheap. Show me the code.", "Do not go gentle into that good night."];
-    var by = ["Tim Bergling", "Ada Lovelace", "Linus Torvalds", "Dylan Thomas"]
+    var by = ["Tim Bergling", "Ada Lovelace", "Linus Torvalds", "Dylan Thomas"];
     frame = document.getElementById("welcome");
     title = document.getElementById("startify");
     author = document.getElementById("author");
