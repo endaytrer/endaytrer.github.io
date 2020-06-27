82d8563fb47e102c8f88576b56a910c7d57fe4c1
endaytrer <endaytrer@gmail.com>
2020-06-28 00:21:55 +0800

    reset dev

--- purple/script.js
+++
@@ -1,9 +0,0 @@
-$("document").ready(function () {
-    var qrCode = new QRCode("qrCode", {
-        text: "               " + "AC5BFE246FA1451C1BD85C6B2AEDCF" + "               ",
-        width: 150,
-        height: 150,
-        colorDark: "#f97afb",
-        correctLevel: QRCode.CorrectLevel.H
-    });
-})
