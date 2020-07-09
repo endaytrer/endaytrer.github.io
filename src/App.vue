a2ae93c5b339b4a2fcd9fd1743fa177ff032a3f0
endaytrer <endaytrer@gmail.com>
2020-07-09 16:51:35 +0800

    reinitialize vue project

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,28 @@
 <template>
   <div id="app">
-    <div class="colorBackground" :class="{show:!isFirstPage}"></div>
-    <div class="columnContainer">
-      <Profile :class="{noBackground: isFirstPage}" />
-      <Navibar v-model="selection" :class="{noBackground: isFirstPage}" />
-    </div>
-    <div class="mainPanel" :class="{noBackgroundAndShadow: isFirstPage}">
-      <Startify v-if="isFirstPage" />
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Navibar from "./components/Navibar.vue";
-import Profile from "./components/Profile.vue";
-import Startify from "./components/Startify.vue";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
-  data: function() {
-    return {
-      selection: 0
-    };
-  },
-  computed: {
-    isFirstPage: function() {
-      return this.selection === 0;
-    }
-  },
+  name: 'App',
   components: {
-    Navibar,
-    Profile,
-    Startify
+    HelloWorld
   }
-};
+}
 </script>
 
 <style>
-:root {
-  --main-background-color: #fff;
-  --startify-background: url("./assets/lightBackground.jpg");
-  --panel-background-color: rgba(248, 248, 248, 0.6);
-  --sec-background-color: rgba(233, 233, 233, 0.8);
-
-  /* Foregrounds */
-  --main-foreground-color: #2c3e50;
-  --sec-foreground-color: #666;
-
-  --corner-radius: 25px;
-}
-@media only screen and (prefers-color-scheme: dark) {
-  :root {
-    --main-background-color: #232425;
-
-    --startify-background: url("./assets/darkBackground.jpg");
-    --panel-background-color: rgba(40, 40, 40, 0.6);
-    --sec-background-color: rgba(45, 45, 45, 0.8);
-
-    --main-foreground-color: #eee;
-    --sec-foreground-color: #ccc;
-  }
-}
 #app {
-  /* background-image: url("https://images.unsplash.com/photo-1593642633279-1796119d5482?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"); */
-  font-family: "SF Pro Display", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  align-items: center;
-  justify-content: center;
-  color: var(--main-foreground-color);
-  background-image: var(--startify-background);
-  background-size: cover;
-  transition: all 300ms ease-out;
-}
-.colorBackground {
-  z-index: 0;
-  position: absolute;
-  opacity: 0;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  background-color: var(--main-background-color);
-  transition: all 0.3s ease-out;
-}
-
-.columnContainer {
-  padding: 0px;
-  display: flex;
-  flex-direction: column;
-}
-.mainPanel {
-  z-index: 10;
-  transition: all ease-out 0.3s;
-  width: 60vw;
-  min-height: calc(60vh + 140px);
-  background-color: var(--panel-background-color);
-  border-radius: var(--corner-radius);
-  box-shadow: 2px 8px 16px rgba(0, 0, 0, 0.07);
-  margin: 20px 40px;
-  display: flex;
-  flex-direction: column;
-}
-
-.show {
-  opacity: 1;
-}
-
-.noBackgroundAndShadow {
-  background-color: transparent !important;
-  box-shadow: none;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,29 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// Font Awesome
-import {
-  library
-} from '@fortawesome/fontawesome-svg-core'
-import {
-  faUserSecret
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  FontAwesomeIcon
-} from '@fortawesome/vue-fontawesome'
-import {
-  faFontAwesome,
-  faGithub,
-  faTwitter
-} from '@fortawesome/free-brands-svg-icons'
-library.add(faUserSecret,
-  faFontAwesome,
-  faGithub,
-  faTwitter
-)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
 
 new Vue({
